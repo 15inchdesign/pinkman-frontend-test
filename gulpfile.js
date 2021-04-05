a189c67850345f78f0af7970c64a30384f8da50d
Denis <15inchdesign@users.noreply.github.com>
2021-04-05 21:17:41 +0200

    Added minify tasks HTML CSS JS

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,29 @@ const {
   watch,
   series
 } = require('gulp');
+const htmlmin = require('gulp-htmlmin');
 const sass = require('gulp-sass');
 const postcss = require('gulp-postcss');
-const cssnano = require('cssnano');
-const concat = require('gulp-concat');
-const gulpTerser = require('gulp-terser');
-const terser = require('terser');
+const cssnano = require('cssnano'); 
+const minify = require('gulp-minify');
 const imagemin = require('gulp-imagemin');
 const browsersync = require('browser-sync').create();
+
+
+
+// Minify HTML
+function htmlTask() {
+  return src('index.html', {
+
+    })
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(dest('dist'));
+}
 
 // Sass Task
 function scssTask() {
   return src('dev/scss/custom.scss', {
-      sourcemaps: true
+     // sourcemaps: true
     })
     .pipe(sass())
     .pipe(postcss([cssnano]))
@@ -32,30 +42,19 @@ function imgTask() {
     .pipe(dest('./dist/images'))
 }
 
-// Bundel JS
-function jsPack() {
-  return src(['./dev/js/*.js',
-      './dev/js/script.js',
-      'node_modules/jquery/dist/jquery.min.js',
-      'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
-    ], {
-      sourcemaps: true
-    })
-    .pipe(concat('bundle.js'))
-    .pipe(dest('dist/js', {
-      sourcemaps: '.'
-    }));
-}
-
-// JavaScript Task
-function jsTask() {
-  return src('dev/js/script.js', {
-      sourcemaps: true
-    })
-    .pipe(gulpTerser({}, terser.minify))
-    .pipe(dest('dist/js', {
-      sourcemaps: '.'
-    }));
+// Minify JS
+function minTask(){
+  return src(['dev/js/script.js',
+  'node_modules/jquery/dist/jquery.js',
+  'node_modules/bootstrap/dist/js/bootstrap.js',
+  'node_modules/swiper/swiper-bundle.js'])
+  .pipe(minify({
+    ext: {
+        min: '.min.js'
+    },
+    ignoreFiles: ['-min.js']
+}))
+.pipe(dest('dist/js'));
 }
 
 // Browsersync Tasks
@@ -76,14 +75,14 @@ function browsersyncReload(cb) {
 // Watch Task
 function watchTask() {
   watch('*.html', browsersyncReload);
-  watch(['dev/scss/**/*.scss', 'dev/js/**/*.js'], series(scssTask, jsTask, imgTask, jsPack, browsersyncReload));
+  watch(['dev/scss/**/*.scss', 'dev/js/**/*.js'], series(htmlTask, scssTask, imgTask, minTask, browsersyncReload));
 }
 
 // Default Gulp task
 exports.default = series(
+  htmlTask,
   scssTask,
-  jsTask,
-  jsPack,
+  minTask,
   imgTask,
   browsersyncServe,
   watchTask
